dda6b12f2a6e769cec4636286ed0e7d7d3995330
buy22 <90930892+buy22@users.noreply.github.com>
2021-10-23 00:25:38 -0500

    add workflow for mongoDB

--- mongoDB.py
+++ mongoDB.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 import pandas as pd
-
+import time
 
 class MongoDB:
     def __init__(self, db, collection):
@@ -10,15 +10,41 @@ class MongoDB:
 
     def all_data(self):
         # data = pd.DataFrame(list(self.collection.find()))
-        data = pd.DataFrame(list(self.collection.find({}, {"_id": 0}).limit(20)))
+        show = {"_id": 0, "distinguished": 0}
+        data = pd.DataFrame(list(self.collection.find({}, show).limit(20)))
         return data
 
     def workflow_step1(self, cond):
+        assert cond[0] >= 0
+        assert cond[1] >= 0
+        strict_conditions = {}
+        inspection_conditions = {}
+        # score >
+        if cond[0]:
+            strict_conditions["score"] = {"$gt": cond[0]}
+            inspection_conditions["score"] = {"$gt": max(cond[0]-10, 0)}
+        # controversiality <
+        if cond[1]:
+            strict_conditions["controversiality"] = {"lt": cond[1]}
+            inspection_conditions["controversiality"] = {"lt": cond[1]+2}
+        # author =
+        if cond[2]:
+            strict_conditions["author"] = {cond[2]}
+            inspection_conditions["author"] = {cond[2]}
+        # partial text search in body
+        if cond[3]:
+            self.collection.create_index([('body', 'text')])
+            a = '\"' + cond[3] + '\"'
+            strict_conditions["$text"] = {"$search": a}
+            inspection_conditions["$text"] = {"$search": a}
+
+        show = {"_id": 0, "distinguished": 0}
         try:
-            strict_data = pd.DataFrame(list(self.collection.find({"score": {"$gt": cond[0]}},
-                                                                 {"_id": 0}).limit(20)))
+            strict_data = pd.DataFrame(
+                list(self.collection.find(strict_conditions, show).limit(20)))
             inspection_data = pd.DataFrame(
-                list(self.collection.find({"score": {"$gt": max(cond[0]-10, 0)}}, {"_id": 0}).limit(20)))
+                list(self.collection.find(inspection_conditions, show).limit(20)))
+            time.sleep(3)
             return strict_data, inspection_data, True
         except Exception as ex:
             return None, None, False
@@ -26,6 +52,7 @@ class MongoDB:
     def workflow_step2(self, strict_data, inspection_data):
         try:
             strict_data.append(inspection_data)
+            time.sleep(3)
             return strict_data, True
         except Exception as ex:
             return None, False
@@ -33,6 +60,7 @@ class MongoDB:
     def workflow_step3(self, data, attributes):
         try:
             df = data[data.columns.intersection(attributes)]
+            time.sleep(3)
             return df, True
         except Exception as ex:
             return None, False
